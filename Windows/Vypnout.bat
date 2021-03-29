b11c4675e3aab7d3caa074da1d020454acecb920
Miraficus <63237821+miraficus@users.noreply.github.com>
2021-03-29 18:29:10 +0200

    Český soubor

--- Windows/Vypnout.bat
+++ Windows/Vypnout.bat
@@ -1,16 +1,16 @@
 @echo off
-TITLE Timed PC Shutdown
+TITLE Casovane vypnuti pocitace Shutdown
 :start
 CLS
-ECHO [31mShutdown your pc in:
+ECHO [31mVypnout pocitac za:
 ECHO [30m.[0m
-ECHO [32m1[0m  [33m15 MINUTES
-ECHO [32m2  [33m30 MINUTES
-ECHO [32m3  [33m1 HOUR
-ECHO [32m4  [33m2 HOURS
-ECHO [32m5  [33m4 HOURS
-ECHO [32mA  [31mAbort shutdown!
-ECHO [32mE  [36mEnd program![0m
+ECHO [32m1[0m  [33m15 MINUT
+ECHO [32m2  [33m30 MINUT
+ECHO [32m3  [33m1 HODINU
+ECHO [32m4  [33m2 HODINY
+ECHO [32m5  [33m4 HODINY
+ECHO [32mA  [31mPrerusit vypnuti!
+ECHO [32mE  [36mUkoncit program![0m
 
 @CHOICE /C:12345AE /N
 IF ERRORLEVEL 7 GOTO end
@@ -24,42 +24,42 @@ GOTO end
 
 :one
 CLS
-ECHO [31mYou have selected [33m15 MINUTES![36m
+ECHO [31mZvolili jste [33m15 MINUT![36m
 shutdown /s /t 900
 timeout 5
 GOTO start
 
 :two
 CLS
-ECHO [31mYou have selected [33m30 MINUTES![36m
+ECHO [31mZvolili jste [33m30 MINUT![36m
 shutdown /s /t 1800
 timeout 5
 GOTO start
 
 :three
 CLS
-ECHO [31mYou have selected [33m1 HOUR![36m
+ECHO [31mZvolili jste [33m1 HODINU![36m
 shutdown /s /t 3600
 timeout 5
 GOTO start
 
 :four
 CLS
-ECHO [31mYou have selected [33m2 HOURS![36m
+ECHO [31mZvolili jste [33m2 HODINY![36m
 shutdown /s /t 7200
 timeout 5
 GOTO start
 
 :five
 CLS
-ECHO [31mYou have selected [33m4 HOURS![36m
+ECHO [31mZvolili jste [33m4 HODINY![36m
 shutdown /s /t 14400
 timeout 5
 GOTO start
 
 :abort
 CLS
-ECHO [31mYou have selected [33mAbort shutdown![36m
+ECHO [31mZvolili jste [33mAbort shutdown![36m
 shutdown /a
 timeout 5
 GOTO start
@@ -67,5 +67,5 @@ GOTO start
 :end
 CLS
 ECHO [30m.[0m
-ECHO [31mThis window will be closed![36m
+ECHO [31mToto okno bude zavreno![36m
 timeout 5
