25c15f99210eadff3baf3258df995f4f0762145c
Miraficus <63237821+miraficus@users.noreply.github.com>
2024-12-19 16:30:36 +0100

    better naming

--- Windows/TurnOff.bat
+++ Windows/TurnOff.bat
@@ -15,42 +15,42 @@ ECHO [32mE  [36mEnd program![0m
 @CHOICE /C:12345AE /N
 IF ERRORLEVEL 7 GOTO end
 IF ERRORLEVEL 6 GOTO abort
-IF ERRORLEVEL 5 GOTO five
-IF ERRORLEVEL 4 GOTO four
-IF ERRORLEVEL 3 GOTO three
-IF ERRORLEVEL 2 GOTO two
-IF ERRORLEVEL 1 GOTO one
+IF ERRORLEVEL 5 GOTO 4h
+IF ERRORLEVEL 4 GOTO 2h
+IF ERRORLEVEL 3 GOTO 1h
+IF ERRORLEVEL 2 GOTO 30m
+IF ERRORLEVEL 1 GOTO 15m
 GOTO end
 
-:one
+:15m
 CLS
 ECHO [31mYou have selected [33m15 MINUTES![36m
 shutdown /s /t 900
 timeout 5
 GOTO start
 
-:two
+:30m
 CLS
 ECHO [31mYou have selected [33m30 MINUTES![36m
 shutdown /s /t 1800
 timeout 5
 GOTO start
 
-:three
+:1h
 CLS
 ECHO [31mYou have selected [33m1 HOUR![36m
 shutdown /s /t 3600
 timeout 5
 GOTO start
 
-:four
+:2h
 CLS
 ECHO [31mYou have selected [33m2 HOURS![36m
 shutdown /s /t 7200
 timeout 5
 GOTO start
 
-:five
+:4h
 CLS
 ECHO [31mYou have selected [33m4 HOURS![36m
 shutdown /s /t 14400
